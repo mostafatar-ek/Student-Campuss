a89a1f4100f4d2483efb178926c6537ad4c75816
Mostafa tarek <proytxox@gmail.com>
2026-05-02 22:34:08 +0300

    Add LoginDTO and RegisterDTO

---
+++ student-portal/src/main/java/com/studentportal/dto/LoginDTO.java
@@ -0,0 +1,23 @@
+package com.studentportal.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginDTO {
+
+    @NotBlank
+    @Size(max = 60)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String password;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email.replaceAll(" ", ""); }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password.replaceAll(" ", ""); }
+}
